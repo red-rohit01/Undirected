010340c710e92abcd36976fd200bdde33e8d8116
red-rohit01 <rr42041@gmail.com>
2023-08-13 19:39:48 +0530

    backend url updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,7 @@
-export const server = "http://localhost:8000/api/v2";
+//export const server = "http://localhost:8000/api/v2";
+//export const backend_url = "http://localhost:8000/";
+export const server = "https://undirected.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://undirected.vercel.app/";
+
+
